1feee2d93d43da45a0b3aa1689d25f7e87f595db
Elicia Au Duong <eliciaauduong@outlook.com>
2019-12-09 12:15:37 +1100

    python input and output

---
+++ 03-printandinput/02-inputfun.py
@@ -0,0 +1,13 @@
+'''s = input()
+print(s)
+
+s = input("Enter your name: ")
+print(s)
+
+i = int(input("Enter an integer: "))
+print(i)
+print(type(i))
+'''
+
+lst = [int(x) for x in input("Enter three numbers seperated by comma: ").split(',')]
+print(lst)
