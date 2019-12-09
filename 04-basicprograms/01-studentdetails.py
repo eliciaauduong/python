8ad868304527e9b70c0e05f0a808cc8eddd896b1
Elicia Au Duong <eliciaauduong@outlook.com>
2019-12-09 12:15:52 +1100

    python basic programs

---
+++ 04-basicprograms/01-studentdetails.py
@@ -0,0 +1,5 @@
+studentid = int(input("Enter student ID: "))
+name = input("Enter student name: ")
+marks = float(input("Enter marks scored: "))
+
+print("ID", studentid, "Name", name, "Marks", marks)
