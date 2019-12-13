edde575583f83f768299c401e9408c8955fcc9f9
Elicia Au Duong <eliciaauduong@outlook.com>
2019-12-13 11:48:07 +1100

    python OOP basics

---
+++ 09-oopsbasics/01-product.py
@@ -0,0 +1,11 @@
+class Product:
+    def __init__(self):
+        self.name = 'iPhone'
+        self.description = 'cool'
+        self.price = 700
+    
+p1 = Product()
+
+print(p1.name)
+print(p1.description)
+print(p1.price)
